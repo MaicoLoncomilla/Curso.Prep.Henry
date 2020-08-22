8a167e9920b3ebf049dbeb5989e27c89190c2a52
Maico <maicoloncomilla@gmail.com>
2020-08-22 11:29:21 -0300

    Terminado el ejercicio 6 mas un archivo llamado escplicaicon.

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,38 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  return cb();
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  var x = 0;
+  for(var i = 0; i < numeros.length; i++){
+   x = numeros[i] + x;
+  }
+  return cb(x);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach(function(array){
+    return cb(array);
+  });
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+const x = [];
+
+for(var i = 0; i< array.length; i ++){
+  x.push(cb(array[i]));
+}
+return x;
 }
 
 // No modificar nada debajo de esta línea
